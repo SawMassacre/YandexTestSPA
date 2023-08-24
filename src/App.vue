b19644ac15afe9473b7246e66469be833ad407db
Evgenii Gerasimov <103824860+SawMassacre@users.noreply.github.com>
2023-08-24 19:44:50 +1000

    ✨ feat: implemented brutalism blocks

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <fullscreen-block :color="'#3b71ee'"></fullscreen-block>
+  <fullscreen-block :color="'#fc724c'"></fullscreen-block>
+  <fullscreen-block :color="'#fccb4c'"></fullscreen-block>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FullscreenBlock from "./components/FullscreenBlock.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FullscreenBlock,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/FullscreenBlock.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fullscreen-block" :style="{ backgroundColor: color }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["color"],
+};
+</script>
+
+<style>
+.fullscreen-block {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
